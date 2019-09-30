f48c4b6bf1a859e92adca961e42c65d8b2aeaadd
Stan <s1113370@student.hsleiden.nl>
2019-09-30 11:41:52 +0200

    Added list

--- app/src/main/java/eu/aimless/f1predictor/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/eu/aimless/f1predictor/ui/dashboard/DashboardFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -12,24 +15,44 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import eu.aimless.f1predictor.R;
+import eu.aimless.f1predictor.repository.model.Race;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    private ListView mListView;
+    private RaceListAdapter mAdapter;
+    private List<Race> raceModels = new ArrayList<>();
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
+    //code zoals "courselistactivity" in college 3
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel = ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
+        final TextView textView = root.findViewById(R.id.race_list_view);
         dashboardViewModel.getText().observe(this, new Observer<String>() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onChanged(@Nullable String s) { textView.setText(s);
             }
         });
+
+        mListView = (ListView)findViewById(R.id.race_list_view);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l){
+                Toast t = Toast.makeText(DashboardFragment.this, "Click" + position, Toast.LENGTH_LONG);
+                t.show();
+            }
+        }
+        );
+
+        //DUMMY DATA
+        raceModels.add(new Race("25-7-2019", "Germany", 6, 2019, "14:10", "onbekende URL"));
+        raceModels.add(new Race("17-5-2019", "Australia", 1, 2019, "07:10", "onbekende URL"));
+
         return root;
     }
 }
---
+++ app/src/main/java/eu/aimless/f1predictor/ui/dashboard/RaceListAdapter.java
@@ -0,0 +1,49 @@
+package eu.aimless.f1predictor.ui.dashboard;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import eu.aimless.f1predictor.R;
+import eu.aimless.f1predictor.repository.model.Race;
+
+public class RaceListAdapter extends ArrayAdapter<Race> {
+
+    public RaceListAdapter(Context context, int resource, List<Race> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder vh;
+
+        if(convertView == null) {
+            vh = new ViewHolder();
+            LayoutInflater li = LayoutInflater.from(getContext());
+
+            convertView = li.inflate(R.layout.view_content_row, parent, false);
+            vh.raceName = (TextView) convertView.findViewById(R.id.raceName);
+            vh.round = (TextView) convertView.findViewById(R.id.round);
+            convertView.setTag(vh);
+        }
+        else {
+            vh = (ViewHolder) convertView.getTag();
+        }
+        Race race = getItem(position);
+        vh.raceName.setText((CharSequence) race.getRaceName());
+        vh.round.setText(String.valueOf((CharSequence) race.getRound())); //int moet String worden
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        TextView raceName;
+        TextView round;
+    }
+}
