7b2f69addb7e4207409fc417decc7cf6801a6052
Aimless321 <6079485+Aimless321@users.noreply.github.com>
2019-10-02 20:47:14 +0200

    Return tasks from Firestore

--- app/src/main/java/eu/aimless/f1predictor/repository/Firestore.java
+++ app/src/main/java/eu/aimless/f1predictor/repository/Firestore.java
@@ -1,9 +1,11 @@
 package eu.aimless.f1predictor.repository;
 
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.core.FirestoreClient;
 
 public class Firestore {
@@ -14,11 +16,13 @@ public class Firestore {
         db = FirebaseFirestore.getInstance();
     }
 
-    public CollectionReference getRaceResults() {
-        return db.collection("results/race/2019");
+    public Task<QuerySnapshot> getRaceResults() {
+        return db.collection("results/race/2019").get();
     }
 
-    public Query getLatestRace() {
-        return db.collection("results/race/2019/").orderBy("round", Query.Direction.DESCENDING).limit(1);
+    public Task<QuerySnapshot> getLatestRace() {
+        return db.collection("results/race/2019/")
+                .orderBy("round", Query.Direction.DESCENDING)
+                .limit(1).get();
     }
 }
